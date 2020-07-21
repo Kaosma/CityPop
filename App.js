3c84bffeb853ad41182e4c953b61521983ced34e
Erik Ugarte <erik-nu@hotmail.com>
2020-07-21 09:56:51 +0200

    First layout update
    Printing out the value of the textinput with a decent UI.

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredCity, setEnteredCity] = useState('');
+
+  const inputHandler = (enteredText) => {
+    setEnteredCity(enteredText);
+  };
+
+  const showCityHandler = () => {
+    console.log(enteredCity);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>CityPop</Text>
-      
-      <Button title = "Search" />
+      <Text style = {styles.title}>CityPop</Text>
+
+      <TextInput
+        style = {{
+          backgroundColor: '#ffffff', 
+          height: 30, 
+          width: 200,
+          borderRadius: 5,
+        }} 
+        placeholder="City or country"
+        onChangeText={inputHandler}
+        value = {enteredCity}
+      />
+      <Button title = "Search" onPress={showCityHandler} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,8 +37,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffa41b',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#ffffff',
+    fontSize: 40,
+    fontFamily: 'Avenir',
+    marginBottom: 60,
+  }
 });
